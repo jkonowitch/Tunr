60f4c5fd150921bd1901d4d5f77d533e070fa2d1
Jeffrey Konowitch <jkonowitch@gmail.com>
2013-10-16 15:19:16 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tunr2::Application.routes.draw do
 
   resources :users do
     resources :owned_songs, only: [:index, :show]
-    resources :playlists
+    resources :playlists, shallow: true
   end
 
   resources :artists, only: [:index, :show] do
