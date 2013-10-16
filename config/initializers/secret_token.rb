4c62fcd93366bd0f0f6964fc28751308a59b4ccd
Jeffrey Konowitch <jkonowitch@gmail.com>
2013-10-16 12:32:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tunr2::Application.config.secret_key_base = '01f49b252401c80fdb097a9935f48210a5e42a95073058f71c68377dc21994e82ac4973d392c38141333884c03cb3fb5a64f9f32f8d1d019c385337d698c1201'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tunr2::Application.config.session_store :cookie_store, key: '_tunr2_session'
