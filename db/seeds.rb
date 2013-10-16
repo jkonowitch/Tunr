1235500c467c9450ff601d5cb6ff7df2c9db3e7d
Jeffrey Konowitch <jkonowitch@gmail.com>
2013-10-16 15:11:37 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Song.destroy_all
+Artist.destroy_all
+
 radiohead = Artist.create(name: "Radiohead")
 
-radiohead.songs.create(name: "Fake Plastic Trees")
+radiohead.songs.create(name: "Fake Plastic Trees", spotify_uri: "spotify:track:0Uro2xUVsEHQNKR5QL4Lku")
